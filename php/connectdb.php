e8b5edde3dae6e47beac9f8f838f4112af455acf
loxbezsaita <bot99980@gmail.com>
2026-06-19 13:57:05 +0700

    Update connectdb.php

--- php/connectdb.php
+++ php/connectdb.php
@@ -1,6 +1,6 @@
 <?php 
 	try {
-		$conn = new PDO('mysql:host=localhost;dbname=rumc_spo','root','root');
+		$conn = new PDO('mysql:host=localhost;dbname=rumc_spo','root','');
 	} catch (PDOException $pe) {
 		die("Could not connect to the database $dbname :" . $pe->getMessage());
 	}
